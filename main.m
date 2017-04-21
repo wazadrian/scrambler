389e94b02bebf6c383ecf7c801d497879d6766a2
Unknown <226042@student.pwr.edu.pl>
2017-04-21 19:10:51 +0200

    added histograms

--- main.m
+++ main.m
@@ -6,10 +6,22 @@ scrambledImage = scramble(grayScale);
 descrambledImage = descramble(scrambledImage);
 
 figure('Name','Source Image');
+subplot(2,1,1); 
 imshow(grayScale)
 
+subplot(2,1,2);
+imhist(grayScale)
+
 figure('Name','Scrambled Image');
+subplot(2,1,1); 
 imshow(scrambledImage)
 
+subplot(2,1,2);
+imhist(scrambledImage)
+
 figure('Name','Descrambled Image');
+subplot(2,1,1); 
 imshow(descrambledImage)
+
+subplot(2,1,2);
+imhist(descrambledImage)
